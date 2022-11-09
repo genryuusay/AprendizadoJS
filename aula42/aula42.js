e4fd2f94b8738145e361cdcbc0f4124795b4e57b
genryuusay <ravilx02@gmail.com>
2022-11-09 07:59:39 -0300

    .

---
+++ aula42/aula42.js
@@ -0,0 +1,10 @@
+// function ePaisagem(largura,altura){
+//     return largura > altura;
+// }
+
+// console.log(ePaisagem(1020,999));
+
+
+const ePaisagem = (largura,altura) => largura > altura;
+
+console.log(ePaisagem(1020,1400));
