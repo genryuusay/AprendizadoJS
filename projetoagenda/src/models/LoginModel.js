17d5cedff35c9ee557ed7e3ece1ad2ae7b54e99b
genryuusay <ravilx02@gmail.com>
2023-06-13 17:58:16 -0300

    correção

--- projetoagenda/src/models/LoginModel.js
+++ projetoagenda/src/models/LoginModel.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcryptjs = require('bcryptjs');
 
 const LoginSchema = new mongoose.Schema({
   email: { type: String, required: true },
-  senha: { type: String, required: true }
+  password: { type: String, required: true }
 });
 
 const LoginModel = mongoose.model('Login', LoginSchema);
@@ -19,8 +20,26 @@ class Login {
 async register(){
   this.valida();
   if(this.errors.length > 0) return;
-  this.user =  await LoginModel.create(this.body)
+
+  await this.userExists();
+
+  if(this.errors.length > 0) return;
+
+  const salt = bcryptjs.genSaltSync();
+  this.body.password = bcryptjs.hashSync(this.body.password, salt);
+  
+
+  this.user =  await LoginModel.create(this.body);
+
+ 
 }
+
+async userExists(){
+  const user = await LoginModel.findOne({email: this.body.email});
+
+  if(user) this.errors.push('Usuário já existe');
+}
+
 valida(){
   this.cleanUp();
 
