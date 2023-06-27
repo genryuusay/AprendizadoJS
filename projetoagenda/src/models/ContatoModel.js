5e81872117f67fa2c34c4ef636b4a9b4ed698178
genryuusay <ravilx02@gmail.com>
2023-06-27 15:07:58 -0300

    revisão

--- projetoagenda/src/models/ContatoModel.js
+++ projetoagenda/src/models/ContatoModel.js
@@ -64,6 +64,17 @@ Contato.buscaPorId = async function(id) {
   return contato;
 };
 
+Contato.buscaContatos = async function(){
+  const contatos = await ContatoModel.find().sort({criadosEm: -1});
+  return contatos;
+};
+
+Contato.delete = async function(id){
+  if(typeof id !== 'string') return;
+  const contato = await ContatoModel.findOneAndDelete({_id: id});
+  return contato;
+ 
+};
 
 
 module.exports = Contato;
