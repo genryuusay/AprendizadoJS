e911dce1e7578a074e7e3c00b8cbdfa7435361df
José Henrique <ravilx02@gmail.com>
2024-03-07 11:52:45 -0300

    dashboard header e conteudo

--- billingCycle/frontend/src/main/app.jsx
+++ billingCycle/frontend/src/main/app.jsx
@@ -4,14 +4,14 @@ import React from "react";
 import Header from '../common/template/header'
 import Sidebar from '../common/template/sidebar';
 import Footer from '../common/template/footer';
-
+import Routes from '../main/routes'
 
 export default props => (
     <div className="wrapper">
         <Header />
         <Sidebar/>
         <div className='content-wrapper'>
-            <h1>Conteudo</h1>
+            <Routes/>
         </div>
         <Footer/>
     </div>
--- billingCycle/frontend/src/main/routes.jsx
+++ billingCycle/frontend/src/main/routes.jsx
@@ -8,7 +8,7 @@ import BillingCycle from '../billingCycle/billingCycle'
 export default props => (
     <Router history={hashHistory}>
         <Route path='/' component={Dashboard}/>
-        <Route path='/billingCycle' component={BillingCycle}/>
+        <Route path='/billingCycles' component={BillingCycle}/>
         <Redirect from ='*' to='/'/>
     </Router>
 )
